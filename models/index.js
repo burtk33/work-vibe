daebd3e5c66971434471b4ea8fe0ac20f2579720
burtk33 <burtk33@gmail.com>
2021-03-29 21:13:03 -0400

    Models and DB connection

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const Users = require('./Users');
+const Project = require('./Project');
+
+Users.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { Users, Project };
