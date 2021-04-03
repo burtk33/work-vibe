3ebfc6ea22216373810de0b60c876937c2fa3e54
burtk33 <burtk33@gmail.com>
2021-04-03 10:24:05 -0400

    getting seeds file up to date

--- seeds/userData.js
+++ seeds/userData.js
@@ -3,20 +3,7 @@ const { Users } = require('../models');
 const userData = 
 
 [
-    {
-      "username": "Bob123",
-      "first_name": "Bob",
-      "last_name": "Robert",
-      "password": "password123"
-
-    },
-    {
-      "username": "Bill123",
-      "first_name": "William",
-      "last_name": "Smith",
-      "password": "password123"
-
-    }
+  
   ]
 
   const seedSteps = () => Users.bulkCreate(userData);
