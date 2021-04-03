40039811cf61f0a1ad0ca6f25299ddc0c6cd2abc
burtk33 <burtk33@gmail.com>
2021-04-03 11:10:27 -0400

    adding project collapsible list to dashboard

---
+++ public/js/homepage.js
@@ -0,0 +1,7 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit();
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
