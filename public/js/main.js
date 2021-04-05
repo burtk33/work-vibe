07d5cbf8da2ed9ed39e194d58473bc5ead533d09
Shannon Alexander <shannonmariealexander94@gmail.com>
2021-04-05 18:55:28 -0400

    updated formatting and added landing page

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     M.AutoInit();
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    var instances = M.Sidenav.init(elems);
 });
