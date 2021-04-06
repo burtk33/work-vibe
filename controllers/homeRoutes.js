e7f7290842dee3172140cc04ae93fce25df73557
Morgan Bailey <mabailey079@Gmail.com>
2021-04-05 20:39:27 -0400

    update home route and project view

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,17 @@
-const router = require('express').Router();
-const { Users, Project } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Users, Project, Steps, References } = require("../models");
+const withAuth = require("../utils/auth");
 
 //GET route for homepage
-router.get('/', withAuth, async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
-    const projectData = await Project.findAll({where:{user_id: req.session.user_id}});
+    const projectData = await Project.findAll({
+      where: { user_id: req.session.user_id },
+    });
 
     const projects = projectData.map((project) => project.get({ plain: true }));
 
-    res.render('homepage', {
+    res.render("homepage", {
       projects,
       logged_in: req.session.logged_in,
     });
@@ -18,14 +20,38 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
-router.get('/projects/:id', withAuth, async (req, res) => {
+router.get("/projects/:id", withAuth, async (req, res) => {
   try {
     const projectData = await Project.findByPk(req.params.id, {
-     });
+      include: [
+        {
+          model: Steps,
+          attributes: [
+            "id",
+            "title",
+            "description",
+            "step_number",
+            "progress_id",
+            "project_id",
+            "user_id",
+          ],
+          include: {
+            model: References,
+            attributes: ["id", "reference", "project_id", "user_id", "step_id"],
+          },
+        },
+        {
+          model: Users,
+          attributes: ["id"],
+        },
+
+        
+      ],
+    });
 
     const project = projectData.get({ plain: true });
 
-    res.render('project', {
+    res.render("project", {
       project,
       logged_in: req.session.logged_in,
     });
@@ -35,13 +61,13 @@ router.get('/projects/:id', withAuth, async (req, res) => {
 });
 
 //GET route for login
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
