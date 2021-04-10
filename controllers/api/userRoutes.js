01f6eef14ab394bb71a3e9c8a79a383a567baa00
burtk33 <burtk33@gmail.com>
2021-04-10 09:26:54 -0400

    updating redirect

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -49,9 +49,8 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
+      res.redirect('/');
       
-      res.json({ user: userData, message: 'You are now logged in!' });
-      res.render('homepage')
     });
 
   } catch (err) {
