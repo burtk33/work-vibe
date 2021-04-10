8af91b53614059e0088686718146157399669869
burtk33 <burtk33@gmail.com>
2021-04-10 08:51:37 -0400

    minor edits

--- public/js/login.js
+++ public/js/login.js
@@ -13,7 +13,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.redirect('/');
       } else {
         alert('Failed to log in');
       }
