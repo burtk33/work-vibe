82d478283239a311d8b3bf8bae39bbcb69d6a814
burtk33 <burtk33@gmail.com>
2021-04-10 08:46:22 -0400

    reverting file path

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
   
     if (username && password) {
-      const response = await fetch('http://work-vibe.herokuapp.com/api/users/login', {
+      const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
