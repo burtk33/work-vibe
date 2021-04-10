a2128bdc85e16f37e305c65beb8208127c2a6457
burtk33 <burtk33@gmail.com>
2021-04-10 08:53:58 -0400

    reverting

--- public/js/login.js
+++ public/js/login.js
@@ -13,7 +13,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.redirect('/');
+        document.location.replace('/');
       } else {
         alert('Failed to log in');
       }
